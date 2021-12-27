6e55ec2a66702acb7a1e01106a6644f432e439f3
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-12-27 18:47:26 +0100

    debugger: Full address info

--- debugger/debugger.c
+++ debugger/debugger.c
@@ -2,11 +2,11 @@
 
 void m_printregs()
 {
-	printf("Z0: %x    ", m_zero);
-	
+	printf("Z0: 0x%06X ", m_zero);
+
 	for (size_t m_idx = 0; m_idx < (M_R3000_REGISTERS - 1); m_idx++)
 	{
-		printf("%s: %x    ", m_cpu_regnames[m_idx], m_cpu->m_registers[m_idx]);
+		printf("%s: 0x%06X ", m_cpu_regnames[m_idx], m_cpu->m_registers[m_idx]);
 		
 		if (!(m_idx % 2))
 		{
