25d2bd549bbd816a4250c3c2da277d11793b3743
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-12-28 17:51:35 +0100

    debugger: Colourize output

--- debugger/debugger.c
+++ debugger/debugger.c
@@ -2,11 +2,12 @@
 
 void m_printregs()
 {
-	printf("Z0: 0x%06X ", m_zero);
+	printf(MAGENTA UNDERSCORE "\nGeneral Prupose Registers:\n" WHITE NORMAL);
+	printf(MAGENTA "z0:" NORMAL WHITE " 0x%08X ", m_zero);
 
 	for (size_t m_idx = 0; m_idx < (M_R3000_REGISTERS - 1); m_idx++)
 	{
-		printf("%s: 0x%06X ", m_cpu_regnames[m_idx], m_cpu->m_registers[m_idx]);
+		printf(MAGENTA "%s:" NORMAL WHITE " 0x%08X ", m_cpu_regnames[m_idx], m_cpu->m_registers[m_idx]);
 		
 		if (!(m_idx % 2))
 		{
@@ -14,4 +15,12 @@ void m_printregs()
 		}
 	}
 
+	printf(BLUE UNDERSCORE "\nControl-Flow Registers:\n" WHITE NORMAL);
+	printf(BLUE "PC:" WHITE " 0x%08X\n", PC);
+
+	printf(GREEN UNDERSCORE "\nArithmetic Registers:\n" WHITE NORMAL);
+	printf(GREEN "HI:" WHITE " 0x%08X\n", HI);
+	printf(GREEN "LO:" WHITE " 0x%08X\n", LO);
+
+	printf(YELLOW UNDERSCORE "\nSegment Registers:\n" WHITE NORMAL);
 }
