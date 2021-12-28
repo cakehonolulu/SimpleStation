cf44b66ff4379b9212056dba2191ccc043c4a5fa
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-12-28 17:51:09 +0100

    include/ui: termcolour.h; a way to colourize printf()s

---
+++ include/ui/termcolour.h
@@ -0,0 +1,16 @@
+#ifndef TERMCOLOUR_H
+#define TERMCOLOUR_H
+
+#define NORMAL		"\x1B[0m"
+#define RED			"\x1B[31m"
+#define GREEN		"\x1B[32m"
+#define YELLOW		"\x1B[33m"
+#define BLUE		"\x1B[34m"
+#define MAGENTA		"\x1B[35m"
+#define CYAN		"\x1B[36m"
+#define WHITE		"\x1B[37m"
+
+#define DIM			"\x1B[7m"
+#define UNDERSCORE	"\x1B[4m"
+
+#endif /* TERMCOLOUR_H */
