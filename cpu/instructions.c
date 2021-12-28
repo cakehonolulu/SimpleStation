e4547a655900f3a697f196a5e8e7968f7694c6dc
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-12-28 12:50:59 +0100

    cpu/instr: Add comments on Load-Upper-Immediate

--- cpu/instructions.c
+++ cpu/instructions.c
@@ -70,6 +70,16 @@ const struct m_corewave_cw33300_instrs m_psx_instrs[67] = {
 	{NULL, NULL}	// 0x00
 };
 
+/*
+	LUI (MIPS I)
+	
+	Format: LUI rt, immediate 
+	
+	Description:
+	Load a constant into the upper half of a word.
+	The 16-bit immediate is shifted left 16 bits and concatenated with 16 bits of low-order
+	zeros. The 32-bit result is sign-extended and placed into GPR rt.
+*/
 void m_lui()
 {
 #ifdef DEBUG_INSTRUCTIONS
@@ -79,8 +89,10 @@ void m_lui()
 	// Check if register isn't register zero
 	if (m_regidx)
 	{
+		// Bit shift by 16 the immediate value and place it at the register pointed by the index
 		REGS[REGVAL] = (IMMDT << 16);
 	}
 	
+	// Increment Program Counter by 4
 	PC += 4;
 }
