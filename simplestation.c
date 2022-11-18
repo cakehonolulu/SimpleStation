98eede6c3a16fbe4e36a0a842089459cfd188ca2
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-11-18 16:43:07 +0100

    cpu: Print regs at exit

--- simplestation.c
+++ simplestation.c
@@ -105,6 +105,8 @@ int main(int argc, char **argv)
 
 uint8_t m_simplestation_exit(m_simplestation_state *m_simplestation, uint8_t m_is_fatal)
 {
+	m_printregs(m_simplestation);
+	
 	if (m_simplestation->m_memory_state)
 	{
 		m_memory_exit(m_simplestation);
