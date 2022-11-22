d19a43dc0d76f0fbcc25f761264057d48c609fa3
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-11-22 12:36:54 +0100

    cpu: Add separate table for opcode strings

---
+++ cpu/opcodes.c
@@ -0,0 +1,256 @@
+
+#include <cpu/opcodes.h>
+
+void p_exp(m_simplestation_state *m_simplestation)
+{
+	// Check if the instruction is implemented
+	if (m_psx_extended_00_opcodes[(m_simplestation->m_cpu->m_opcode & 0x3F)].m_funct == NULL)
+	{
+		printf(RED "[CPU] fde->exp: Unimplemented '0x00 Family' Opcode: 0x%02X (Full Opcode: 0x%08X)\n" NORMAL, (m_simplestation->m_cpu->m_opcode & 0x3F), (uint32_t) m_simplestation->m_cpu->m_opcode);
+		m_simplestation_exit(m_simplestation, 1);
+	}
+	else
+	{
+		// Execute the instruction
+		((void (*)(m_simplestation_state *m_simplestation))m_psx_extended_00_opcodes[(m_simplestation->m_cpu->m_opcode & 0x3F)].m_funct)(m_simplestation);
+	}
+}
+
+void p_cop0(m_simplestation_state *m_simplestation)
+{
+	// Check if the instruction is implemented
+	if (m_psx_cop0_opcodes[REGIDX_S].m_funct == NULL)
+	{
+		printf(RED "[CPU] fde->cop0: Unimplemented 'Coprocessor 0' Opcode: 0x%02X (Full Opcode: 0x%08X)\n" NORMAL, REGIDX_S, m_simplestation->m_cpu->m_opcode);
+		m_simplestation_exit(m_simplestation, 1);
+	}
+	else
+	{
+		// Execute the instruction
+		((void (*)(void))m_psx_cop0_opcodes[REGIDX_S].m_funct)();
+	}
+}
+
+void p_mfc0(m_simplestation_state *m_simplestation)
+{
+	printf("mfc0 $%d, $%s\n", REGIDX_T, m_cop0_regnames[REGIDX_D]);
+}
+
+void p_mtc0(m_simplestation_state *m_simplestation)
+{
+	printf("mtc0 $%s, $%s\n", m_cpu_regnames[REGIDX_T], m_cop0_regnames[REGIDX_D]);
+}
+
+void p_bxx(m_simplestation_state *m_simplestation)
+{
+	if ((m_simplestation->m_cpu->m_opcode >> 16) & 1)
+	{
+		if (((m_simplestation->m_cpu->m_opcode >> 17) & 0xF) == 8)
+		{
+			printf("bgezal $%s, %d\n", m_cpu_regnames[REGIDX_S], SIMMDT << 2);
+		}
+		else
+		{
+			printf("bgez $%s, %d\n", m_cpu_regnames[REGIDX_S], SIMMDT << 2);
+		}
+	}
+	else
+	{
+		if (((m_simplestation->m_cpu->m_opcode >> 17) & 0xF) == 8)
+		{
+			printf("bltzal $%s, %d\n", m_cpu_regnames[REGIDX_S], SIMMDT << 2);
+		}
+		else
+		{
+			printf("bltz $%s, %d\n", m_cpu_regnames[REGIDX_S], SIMMDT << 2);
+		}
+	}
+}
+
+void p_sll(m_simplestation_state *m_simplestation)
+{
+	printf("sll $%s, $%s, %x\n", m_cpu_regnames[REGIDX_D], m_cpu_regnames[REGIDX_T], SHIFT);
+}
+
+void p_srl(m_simplestation_state *m_simplestation)
+{
+	printf("srl $%s, $%s, %x\n", m_cpu_regnames[REGIDX_D], m_cpu_regnames[REGIDX_T], SHIFT);
+}
+
+void p_sra(m_simplestation_state *m_simplestation)
+{
+	printf("sra $%s, $%s, %d\n", m_cpu_regnames[REGIDX_D], m_cpu_regnames[REGIDX_T], SHIFT);
+}
+
+void p_jr(m_simplestation_state *m_simplestation)
+{
+	printf("jr $%s\n", m_cpu_regnames[REGIDX_S]);
+}
+
+void p_jalr(m_simplestation_state *m_simplestation)
+{
+	if (REGIDX_D == 31)
+	{
+		printf("jalr $%s\n", m_cpu_regnames[REGIDX_S]);
+	}
+	else
+	{
+		printf("jalr $%s, $%s\n", m_cpu_regnames[REGIDX_D], m_cpu_regnames[REGIDX_S]);
+	}
+}
+
+void p_syscall(m_simplestation_state *m_simplestation)
+{
+    (void) (m_simplestation);
+    printf("syscall\n");
+}
+
+void p_mfhi(m_simplestation_state *m_simplestation)
+{
+	printf("mfhi $%s\n", m_cpu_regnames[REGIDX_D]);
+}
+
+void p_mflo(m_simplestation_state *m_simplestation)
+{
+	printf("mflo $%s\n", m_cpu_regnames[REGIDX_D]);
+}
+
+void p_div(m_simplestation_state *m_simplestation)
+{
+	printf("div $%s, $%s\n", m_cpu_regnames[REGIDX_S], m_cpu_regnames[REGIDX_T]);
+}
+
+void p_divu(m_simplestation_state *m_simplestation)
+{
+	printf("divu $%s, $%s\n", m_cpu_regnames[REGIDX_S], m_cpu_regnames[REGIDX_T]);
+}
+
+void p_add(m_simplestation_state *m_simplestation)
+{
+	printf("add $%s, $%s, $%s\n", m_cpu_regnames[REGIDX_D], m_cpu_regnames[REGIDX_S], m_cpu_regnames[REGIDX_T]);
+}
+
+void p_addu(m_simplestation_state *m_simplestation)
+{
+	printf("addu $%s, $%s, $%s\n", m_cpu_regnames[REGIDX_D], m_cpu_regnames[REGIDX_S], m_cpu_regnames[REGIDX_T]);
+}
+
+void p_subu(m_simplestation_state *m_simplestation)
+{
+	printf("subu $%s, $%s, $%s\n", m_cpu_regnames[REGIDX_D], m_cpu_regnames[REGIDX_S], m_cpu_regnames[REGIDX_T]);
+}
+
+void p_and(m_simplestation_state *m_simplestation)
+{
+	printf("and $%s, $%s, $%s\n", m_cpu_regnames[REGIDX_D], m_cpu_regnames[REGIDX_S], m_cpu_regnames[REGIDX_T]);
+}
+
+void p_or(m_simplestation_state *m_simplestation)
+{
+	printf("or $%s, $%s, $%s\n", m_cpu_regnames[REGIDX_D], m_cpu_regnames[REGIDX_S], m_cpu_regnames[REGIDX_T]);
+}
+
+void p_slt(m_simplestation_state *m_simplestation)
+{
+	printf("slt $%s, $%s, $%s\n", m_cpu_regnames[REGIDX_D], m_cpu_regnames[REGIDX_S], m_cpu_regnames[REGIDX_T]);
+}
+
+void p_sltu(m_simplestation_state *m_simplestation)
+{
+	printf("sltu $%s, $%s, $%s\n", m_cpu_regnames[REGIDX_D], m_cpu_regnames[REGIDX_S], m_cpu_regnames[REGIDX_T]);
+}
+
+void p_j(m_simplestation_state *m_simplestation)
+{
+	printf("j 0x%x\n", ((PC & 0xF0000000) | (JIMMDT * 4)));
+}
+
+void p_jal(m_simplestation_state *m_simplestation)
+{
+	printf("jal 0x%x\n", ((PC & 0xF0000000) | (JIMMDT * 4)));
+}
+
+void p_beq(m_simplestation_state *m_simplestation)
+{
+	printf("beq $%s, $%s, %d\n", m_cpu_regnames[REGIDX_S], m_cpu_regnames[REGIDX_T], (SIMMDT) << 2);
+}
+
+void p_bne(m_simplestation_state *m_simplestation)
+{
+	printf("bne $%s, $%s, %d\n", m_cpu_regnames[REGIDX_S], m_cpu_regnames[REGIDX_T], (SIMMDT) << 2);
+}
+
+void p_blez(m_simplestation_state *m_simplestation)
+{
+	printf("blez $%s, %d\n", m_cpu_regnames[REGIDX_S], (SIMMDT) << 2);
+}
+
+void p_bgtz(m_simplestation_state *m_simplestation)
+{
+	printf("bgtz $%s, %d\n", m_cpu_regnames[REGIDX_S], (SIMMDT) << 2);
+}
+
+void p_addi(m_simplestation_state *m_simplestation)
+{
+	printf("addi $%s, $%s, %d\n", m_cpu_regnames[REGIDX_S], m_cpu_regnames[REGIDX_T], SIMMDT);
+}
+
+void p_addiu(m_simplestation_state *m_simplestation)
+{
+	printf("addiu $%s, $%s, 0x%X\n", m_cpu_regnames[REGIDX_T], m_cpu_regnames[REGIDX_S], SIMMDT);
+}
+
+void p_slti(m_simplestation_state *m_simplestation)
+{
+	printf("slti $%s, $%s, %d\n", m_cpu_regnames[REGIDX_T], m_cpu_regnames[REGIDX_S], SIMMDT);
+}
+
+void p_sltiu(m_simplestation_state *m_simplestation)
+{
+	printf("sltiu $%s, $%s, %d\n", m_cpu_regnames[REGIDX_T], m_cpu_regnames[REGIDX_S], SIMMDT);
+}
+
+void p_lb(m_simplestation_state *m_simplestation)
+{
+	printf("lb $%s, %d($%s)\n", m_cpu_regnames[REGIDX_T], SIMMDT, m_cpu_regnames[REGIDX_S]);
+}
+
+void p_lw(m_simplestation_state *m_simplestation)
+{
+	printf("lw $%s, 0x%x($%x)\n", m_cpu_regnames[REGIDX_T], IMMDT, REGIDX_S);
+}
+
+void p_lbu(m_simplestation_state *m_simplestation)
+{
+	printf("lbu $%s, %04X($%s)\n", m_cpu_regnames[REGIDX_T], (uint16_t) (SIMMDT & 0x0000FFFF), m_cpu_regnames[REGIDX_S]);
+}
+
+void p_sb(m_simplestation_state *m_simplestation)
+{
+	printf("sb $%s, 0x%x($%s)\n", m_cpu_regnames[REGIDX_T], IMMDT, m_cpu_regnames[REGIDX_S]);
+}
+
+void p_sh(m_simplestation_state *m_simplestation)
+{
+	printf("sh $%s, 0x%x($%s)\n", m_cpu_regnames[REGIDX_T], IMMDT, m_cpu_regnames[REGIDX_S]);
+}
+
+void p_sw(m_simplestation_state *m_simplestation)
+{
+	printf("sw $%s, 0x%X($%s)\n", m_cpu_regnames[REGIDX_T], SIMMDT, m_cpu_regnames[REGIDX_S]);
+}
+
+void p_andi(m_simplestation_state *m_simplestation)
+{
+	printf("andi $%s, $%s, 0x%X\n", m_cpu_regnames[REGIDX_T], m_cpu_regnames[REGIDX_S], IMMDT);
+}
+
+void p_ori(m_simplestation_state *m_simplestation)
+{
+	printf("ori $%s, $%s, 0x%X\n", m_cpu_regnames[REGIDX_T], m_cpu_regnames[REGIDX_S], IMMDT);
+}
+
+void p_lui(m_simplestation_state *m_simplestation)
+{
+	printf("lui $%s, 0x%X\n", m_cpu_regnames[REGIDX_T], IMMDT);
+}
---
+++ include/cpu/opcodes.h
@@ -0,0 +1,151 @@
+#ifndef OPCODES_H
+#define OPCODES_H
+
+#include <cpu/cop0.h>
+#include <memory/memory.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+extern struct m_opc_names {
+	const char *m_instr;
+	void *m_funct;
+} const m_psx_instrs_opcodes[0x3F];
+
+extern struct m_opc_ext_names {
+	const char *m_instr;
+	void *m_funct;
+} const m_psx_extended_00_opcodes[0x3F];
+
+extern struct m_opc_cop0_names {
+	const char *m_instr;
+	void *m_funct;
+} const m_psx_cop0_opcodes[0x5];
+
+/* Instructions */
+
+// COP0
+void p_mfc0(m_simplestation_state *m_simplestation);
+
+void p_mtc0(m_simplestation_state *m_simplestation);
+
+// Exception handler
+void p_exception(m_exc_types m_exception, m_simplestation_state *m_simplestation);
+
+// 0x00
+void p_exp(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x00
+void p_sll(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x02
+void p_srl(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x03
+void p_sra(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x08
+void p_jr(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x09
+void p_jalr(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x0C
+void p_syscall(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x10
+void p_mfhi(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x12
+void p_mflo(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x1A
+void p_div(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x1B
+void p_divu(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x20
+void p_add(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x21
+void p_addu(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x23
+void p_subu(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x24
+void p_and(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x25
+void p_or(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x2A
+void p_slt(m_simplestation_state *m_simplestation);
+
+// 0x00 -> 0x2B
+void p_sltu(m_simplestation_state *m_simplestation);
+
+// 0x01
+void p_bxx(m_simplestation_state *m_simplestation);
+
+// 0x02
+void p_j(m_simplestation_state *m_simplestation);
+
+// 0x03
+void p_jal(m_simplestation_state *m_simplestation);
+
+// 0x04
+void p_beq(m_simplestation_state *m_simplestation);
+
+// 0x05
+void p_bne(m_simplestation_state *m_simplestation);
+
+// 0x06
+void p_blez(m_simplestation_state *m_simplestation);
+
+// 0x07
+void p_bgtz(m_simplestation_state *m_simplestation);
+
+// 0x08
+void p_addi(m_simplestation_state *m_simplestation);
+
+// 0x09
+void p_addiu(m_simplestation_state *m_simplestation);
+
+// 0x0A
+void p_slti(m_simplestation_state *m_simplestation);
+
+// 0x0B
+void p_sltiu(m_simplestation_state *m_simplestation);
+
+// 0x10
+void p_cop0(m_simplestation_state *m_simplestation);
+
+// 0x0C
+void p_andi(m_simplestation_state *m_simplestation);
+
+// 0x0D
+void p_ori(m_simplestation_state *m_simplestation);
+
+// 0x0F
+void p_lui(m_simplestation_state *m_simplestation);
+
+// 0x20
+void p_lb(m_simplestation_state *m_simplestation);
+
+// 0x23
+void p_lw(m_simplestation_state *m_simplestation);
+
+// 0x24
+void p_lbu(m_simplestation_state *m_simplestation);
+
+// 0x28
+void p_sb(m_simplestation_state *m_simplestation);
+
+// 0x29
+void p_sh(m_simplestation_state *m_simplestation);
+
+// 0x2B
+void p_sw(m_simplestation_state *m_simplestation);
+
+#endif /* OPCODES_H */
