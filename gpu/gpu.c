a5ff0f4c0f8b9ac6ccf61332eff9dfff9794640b
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-12-01 14:45:35 +0100

    gpu: Decode GPU status register

--- gpu/gpu.c
+++ gpu/gpu.c
@@ -24,6 +24,73 @@ uint8_t m_gpu_init(m_simplestation_state *m_simplestation)
     return m_result;
 }
 
+uint32_t m_gpu_get_status(m_simplestation_state *m_simplestation)
+{
+    uint32_t m_status = 0;
+    m_status |= m_simplestation->m_gpu->m_page_base_x << 0;
+    m_status |= m_simplestation->m_gpu->m_page_base_y << 4;
+    m_status |= ((uint32_t) m_simplestation->m_gpu->m_semitransparency) << 5;
+    m_status |= ((uint32_t) m_simplestation->m_gpu->m_texture_depth) << 7;
+    m_status |= ((uint32_t) m_simplestation->m_gpu->m_dithering) << 9;
+    m_status |= ((uint32_t) m_simplestation->m_gpu->m_draw_to_display) << 10;
+    m_status |= ((uint32_t) m_simplestation->m_gpu->m_force_set_mask_bit) << 11;
+    m_status |= ((uint32_t) m_simplestation->m_gpu->m_preserve_masked_pixels) << 12;
+    m_status |= ((uint32_t) m_simplestation->m_gpu->m_field) << 13;
+    m_status |= ((uint32_t) m_simplestation->m_gpu->m_texture_disable) << 15;
+    m_status |= ((uint32_t) m_simplestation->m_gpu->m_horizontal_resolution) << 16;
+    m_status |= ((uint32_t) m_simplestation->m_gpu->m_vertical_resolution) << 19;
+    m_status |= ((uint32_t) m_simplestation->m_gpu->m_video_mode) << 20;
+    m_status |= ((uint32_t) m_simplestation->m_gpu->m_display_depth) << 21;
+    m_status |= ((uint32_t) m_simplestation->m_gpu->m_interlaced) << 22;
+    m_status |= ((uint32_t) m_simplestation->m_gpu->m_display_disabled) << 23;
+    m_status |= ((uint32_t) m_simplestation->m_gpu->m_interrupt) << 24;
+
+    // HACK
+    // GPU Always Ready to Recieve Commands
+    m_status |= 1 << 26;
+
+    // Ready to send VRAM to CPU
+    m_status |= 1 << 27;
+
+    // Ready to recieve DMA block
+    m_status |= 1 << 28;
+
+    m_status |= ((uint32_t) m_simplestation->m_gpu->m_dma_direction) << 29;
+
+    m_status |= 0 << 31;
+
+
+    switch (m_simplestation->m_gpu->m_dma_direction)
+    {
+        case off:
+            m_status |= ((0) << 25);
+            break;
+
+        case fifo:
+            m_status |= ((1)  << 25);
+            break;
+
+        case cpu_to_gp0:
+            m_status |= (((m_status >> 28) & 1) << 25);
+            break;
+
+        case vram_to_cpu:
+            m_status |= (((m_status >> 27) & 1) << 25);
+            break;
+
+        default:
+            __builtin_unreachable();
+            break;
+    }
+
+    return m_status;
+}
+
+uint8_t m_gpu_set_horizontal_res(uint8_t m_hoz_res1, uint8_t m_hoz_res2)
+{
+    return ((m_hoz_res2 & 1) | ((m_hoz_res1 & 3) << 1));
+}
+
 void m_gpu_exit(m_simplestation_state *m_simplestation)
 {
     if (m_simplestation->m_gpu_state)
--- include/gpu/gpu.h
+++ include/gpu/gpu.h
@@ -7,6 +7,10 @@
 
 /* Function definitions */
 uint8_t m_gpu_init(m_simplestation_state *m_simplestation);
+
+uint32_t m_gpu_get_status(m_simplestation_state *m_simplestation);
+uint8_t m_gpu_set_horizontal_res(uint8_t m_hoz_res1, uint8_t m_hoz_res2);
+
 void m_gpu_exit(m_simplestation_state *m_simplestation);
 
 #endif /* GPU_H */
