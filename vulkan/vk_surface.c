94ed5b0553d749b7beb13dc85a473f9acdee0e12
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2023-03-03 12:28:05 +0100

    vulkan: Check for compatible surface format

--- vulkan/vk_surface.c
+++ vulkan/vk_surface.c
@@ -2,6 +2,7 @@
 
 void vk_surface_prepare(vulcano_struct *vulcano_state)
 {
+	bool compatible_format = false;
 	vkGetPhysicalDeviceSurfaceCapabilitiesKHR(*vulcano_state->phys_dev, vulcano_state->surface, &vulcano_state->vk_surface_capabilities);
 	
     uint32_t vk_surface_fmt_num = 0;
@@ -10,7 +11,22 @@ void vk_surface_prepare(vulcano_struct *vulcano_state)
     VkSurfaceFormatKHR *vk_surface_fmts = malloc(sizeof(VkSurfaceFormatKHR) * vk_surface_fmt_num);
 	vkGetPhysicalDeviceSurfaceFormatsKHR(*vulcano_state->phys_dev, vulcano_state->surface, &vk_surface_fmt_num, vk_surface_fmts);
 	
+	printf(BOLD GREEN "[vulkan] vk_surface_prepare: Available Surface Formats:" NORMAL "\n");
+
+	for (uint32_t i = 0; i < vk_surface_fmt_num; i++)
+	{
+		if (vk_surface_fmts[i].format == VK_FORMAT_B8G8R8A8_UNORM) compatible_format = true;
+
+		printf(GREEN "- %d" NORMAL "\n", vk_surface_fmts[i].format);
+	}
+
     vulcano_state->vk_surface_format = vk_surface_fmts[0];
+
+	if (compatible_format)
+	{
+		printf(BOLD GREEN "[vulkan] vk_surface_prepare: Setting compatible surface format!" NORMAL "\n");
+		vulcano_state->vk_surface_format.format = VK_FORMAT_B8G8R8A8_UNORM;
+	}
 
 	free(vk_surface_fmts);
 
