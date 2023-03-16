cbd0138abd2b0e315de7371c9cc6e32f9e1c55cf
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2023-03-16 19:36:45 +0100

    spu: Disable logging and crash

--- spu/spu.c
+++ spu/spu.c
@@ -8,8 +8,8 @@ uint32_t m_spu_read(uint32_t m_address, m_simplestation_state *m_simplestation)
     switch (m_address)
     {
         default:
-            printf(RED "[SPU] read: Unhandled SPU read (Address: 0x%08X)\n" NORMAL, m_address);
-            m_simplestation_exit(m_simplestation, 1);
+            //printf(RED "[SPU] read: Unhandled SPU read (Address: 0x%08X)\n" NORMAL, m_address);
+            //m_simplestation_exit(m_simplestation, 1);
             break;
     }
 
@@ -21,8 +21,8 @@ void m_spu_write(uint32_t m_address, uint32_t m_value, m_simplestation_state *m_
     switch (m_address)
     {
         default:
-            printf(RED "[SPU] write: Unhandled SPU write (Address: 0x%08X, Value: 0x%08X)\n" NORMAL, m_address, m_value);
-            m_simplestation_exit(m_simplestation, 1);
+            //printf(RED "[SPU] write: Unhandled SPU write (Address: 0x%08X, Value: 0x%08X)\n" NORMAL, m_address, m_value);
+            //m_simplestation_exit(m_simplestation, 1);
             break;
     }
 }
